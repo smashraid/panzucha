32953e7946885d944682326cbabbfd3a8596d842
Saulo Tsuchida Cruz <smashraid@gmail.com>
2026-05-29 18:31:52 -0500

    feat(sql): updated initial script

--- internal/domain/order.go
+++ internal/domain/order.go
@@ -16,9 +16,9 @@ const (
 )
 
 type OrderItem struct {
-	ProductID string  `json:"product_id"`
-	Quantity  int     `json:"quantity"`
-	UnitPrice float64 `json:"unit_price"`
+	ProductID string
+	Quantity  int
+	UnitPrice float64
 }
 
 type Order struct {
